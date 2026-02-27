48eab1492a44ceadcbbae046b8181ccab94399b2
BitaPatrick <bitapatrick1@gmail.com>
2026-02-27 10:11:28 +0100

    Initial Django photo album

---
+++ album/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Photo
+from .forms import PhotoForm
+
+def photo_list(request):
+    sort = request.GET.get("sort", "date")
+    if sort == "name":
+        photos = Photo.objects.order_by("name")
+    else:
+        photos = Photo.objects.order_by("-uploaded_at")
+    return render(request, "album/photo_list.html", {"photos": photos, "sort": sort})
+
+def photo_detail(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    return render(request, "album/photo_detail.html", {"photo": photo})
+
+def photo_upload(request):
+    if request.method == "POST":
+        form = PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("photo_list")
+    else:
+        form = PhotoForm()
+    return render(request, "album/photo_form.html", {"form": form})
+
+def photo_delete(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    if request.method == "POST":
+        photo.delete()
+        return redirect("photo_list")
+    return render(request, "album/photo_delete.html", {"photo": photo})
