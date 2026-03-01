62304b0d30d7b3be918b7f453f5d35f34173fa00
BitaPatrick <bitapatrick1@gmail.com>
2026-03-01 19:59:00 +0100

    Add image_url + make image optional

--- album/models.py
+++ album/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 class Photo(models.Model):
     name = models.CharField(max_length=40)
+
+    # régi mező megmarad, de opcionálisra tesszük
+    image = models.ImageField(upload_to="photos/", blank=True, null=True)
+
+    # supabase public url ide megy
     image_url = models.URLField(blank=True)
+
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
